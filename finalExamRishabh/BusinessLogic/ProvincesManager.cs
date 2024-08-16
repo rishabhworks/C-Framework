015344e199f2b6abe7a11bcde42b6af03cf00ac2
Rishabh Atul Mishra <mishrris@shernet.sheridancollege.ca>
2024-08-16 13:20:37 -0400

    Province Manager Completed

--- finalExamRishabh/BusinessLogic/ProvincesManager.cs
+++ finalExamRishabh/BusinessLogic/ProvincesManager.cs
@@ -8,5 +8,22 @@ namespace finalExamRishabh.BusinessLogic
 {
     public class ProvincesManager
     {
+        public List<Province> Provinces
+        { get; private set; }
+
+        public ProvincesManager()
+        {
+            Provinces = new List<Province>();
+        }
+
+        public void AddProvince(Province province)
+        {
+            Provinces.Add(province);
+        }
+
+        public Province GetProvinceById(int id)
+        {
+            return Provinces.FirstOrDefault(p => p.Id == id);
+        }
     }
 }

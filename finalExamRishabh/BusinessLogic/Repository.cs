3238b50a358a81a8c694ce67c0071698f90adc65
Rishabh Atul Mishra <mishrris@shernet.sheridancollege.ca>
2024-08-16 13:22:37 -0400

    Repository Class Created

--- finalExamRishabh/BusinessLogic/Repository.cs
+++ finalExamRishabh/BusinessLogic/Repository.cs
@@ -8,6 +8,29 @@ namespace finalExamRishabh.BusinessLogic
 {
     public class Repository
     {
+        public List
+       <Province> LoadProvinces(string filePath)
+        {
+            var provinces = new List
+                <Province>();
 
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                var province = new Province(
+                    int.Parse(parts[0]),
+                    parts[1],
+                    parts[2],
+                    parts[3],
+                    int.Parse(parts[4]),
+                    parts[5]
+                );
+
+                provinces.Add(province);
+            }
+
+            return provinces;
+        }
     }
 }
